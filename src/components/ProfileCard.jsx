a76bef0fcfbf26e6809f22d68f01351aa59488a8
bhaskarm <imbhaskaran@gmail.com>
2022-12-04 23:12:01 +0530

    resume download btn

---
+++ src/components/ProfileCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProfileCard = () => {
+  return (
+    <div className="p-10">
+        <div > About Me </div>
+    </div>
+  )
+};
+
+export default ProfileCard;
